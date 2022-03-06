c98e2ada55d389d92e77ca90a8df1e7c556f7cbb
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-03-06 13:55:22 -0300

    feat: created banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,44 @@
+import Button from 'components/Button'
+import Ribbon from 'components/Ribbon'
+import { RibbonSizes, RibbonColor } from 'components/Ribbon'
+import * as S from './styles'
+
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+  ribbon?: React.ReactNode
+  ribbonSize?: RibbonSizes
+  ribbonColor?: RibbonColor
+}
+
+const Banner = ({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+  ribbon,
+  ribbonSize = 'normal',
+  ribbonColor = 'primary'
+}: BannerProps) => (
+  <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
+    <S.Image src={img} role="img" aria-label={title} />
+    <S.Caption>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+      <Button as="a" href={buttonLink} size="large">
+        {buttonLabel}
+      </Button>
+    </S.Caption>
+  </S.Wrapper>
+)
+
+export default Banner
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,57 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <Banner
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toHaveAttribute(
+      'src',
+      'https://source.unsplash.com/user/willianjusten/1042x580'
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
+})
