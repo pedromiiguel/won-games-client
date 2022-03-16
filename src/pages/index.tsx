a4b74e47b2342f87dfdbb685be62e61cf1e0cc4c
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-03-15 21:29:02 -0300

    feat: created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Main from 'components/Main'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Home() {
-  return <Main />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      banners: bannerMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighlight: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
+    }
+  }
 }
