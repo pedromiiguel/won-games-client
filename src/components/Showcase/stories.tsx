2f08e92b02e1aa70b78d13d76299b41154c7a073
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-03-19 22:11:26 -0300

    feat: created showcase component

---
+++ src/components/Showcase/stories.tsx
@@ -0,0 +1,57 @@
+import { Story, Meta } from '@storybook/react'
+import Showcase, { ShowcaseProps } from '.'
+import highlightMock from 'components/Highlight/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
+export default {
+  title: 'Showcase',
+  component: Showcase,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '0 auto' }}>
+        <Story />
+      </div>
+    )
+  ],
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<ShowcaseProps> = (args) => <Showcase {...args} />
+
+Default.args = {
+  title: 'Most popular',
+  highlight: highlightMock,
+  games: gamesMock
+}
+
+export const WithoutTitle: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+WithoutTitle.args = {
+  highlight: highlightMock,
+  games: gamesMock
+}
+
+export const WithoutHighlight: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+WithoutHighlight.args = {
+  title: 'Most popular',
+  games: gamesMock
+}
+
+export const WithoutGames: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+WithoutGames.args = {
+  title: 'Most popular',
+  highlight: highlightMock
+}
---
+++ src/components/Showcase/test.tsx
@@ -0,0 +1,71 @@
+import 'match-media-mock'
+
+import { screen } from '@testing-library/react'
+import highlightMock from 'components/Highlight/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
+import Showcase from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props = {
+  title: 'Showcase',
+  highlight: highlightMock,
+  games: gamesMock.slice(0, 1)
+}
+
+describe('<Showcase />', () => {
+  it('should render full Showcase', () => {
+    renderWithTheme(<Showcase {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Showcase/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Read Dead it’s back/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getAllByRole('heading', { name: /Population Zero/i })
+    ).toHaveLength(1)
+  })
+
+  it('should without title', () => {
+    renderWithTheme(
+      <Showcase highlight={props.highlight} games={props.games} />
+    )
+
+    expect(
+      screen.queryByRole('heading', { name: /Showcase/i })
+    ).not.toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /Read Dead it’s back/i }))
+
+    expect(screen.getAllByRole('heading', { name: /Population Zero/i }))
+  })
+
+  it('should without highlight', () => {
+    renderWithTheme(<Showcase title={props.title} games={props.games} />)
+
+    expect(screen.getByRole('heading', { name: /Showcase/i }))
+
+    expect(screen.getAllByRole('heading', { name: /Population Zero/i }))
+    expect(
+      screen.queryByRole('heading', { name: /Read Dead it’s back/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should without games', () => {
+    renderWithTheme(
+      <Showcase title={props.title} highlight={props.highlight} />
+    )
+
+    expect(screen.getByRole('heading', { name: /Showcase/i }))
+
+    expect(screen.getByRole('heading', { name: /Read Dead it’s back/i }))
+
+    expect(
+      screen.queryByRole('heading', { name: /Population Zero/i })
+    ).not.toBeInTheDocument()
+  })
+})
