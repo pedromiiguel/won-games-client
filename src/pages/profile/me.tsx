4d75aab67238becf60124eb1a50c5b24db28046a
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-03-27 21:39:30 -0300

    feat: created profile me page

---
+++ src/pages/profile/me.tsx
@@ -0,0 +1,13 @@
+import FormProfile from 'components/FormProfile'
+import React from 'react'
+import Profile from 'templates/Profile'
+
+const ProfileMe = () => {
+  return (
+    <Profile>
+      <FormProfile />
+    </Profile>
+  )
+}
+
+export default ProfileMe
