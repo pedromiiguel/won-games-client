c5a0119e761866ea76571aead1b7bafb4ce21e01
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-03-31 16:10:34 -0300

    feat: created user dropdown component

---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="John Doe" />)
+
+    expect(screen.getByText(/John Doe/)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="John Doe" />)
+
+    userEvent.click(screen.getByText(/John Doe/))
+
+    expect(
+      screen.getByRole('link', { name: /My profile/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /Wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /Sign out/i })).toBeInTheDocument()
+  })
+})
