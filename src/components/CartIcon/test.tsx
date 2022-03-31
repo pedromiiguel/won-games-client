5749564073ecfac0a6f548c4fdb41c3679d5ee61
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-03-31 16:08:22 -0300

    feat: created cart icon component

---
+++ src/components/CartIcon/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import CartIcon from '.'
+
+describe('<CartIcon />', () => {
+  it('should render the without badge', () => {
+    renderWithTheme(<CartIcon />)
+
+    expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
+  })
+
+  it('should render with badge only if has positive numbers', () => {
+    renderWithTheme(<CartIcon quantity={3} />)
+
+    expect(screen.getByLabelText(/cart items/i)).toBeInTheDocument()
+    expect(screen.getByText(/3/)).toBeInTheDocument()
+  })
+
+  it('should not render with negative numbers', () => {
+    renderWithTheme(<CartIcon quantity={-19} />)
+
+    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
+  })
+})
