062e97efc6646ad434e78bf746e33ae34f4dc806
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-05-14 11:33:46 -0300

    feat: connecting page with api

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,8 +3,11 @@ import Cart, { CartTemplateProps } from 'templates/Cart'
 
 import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
+
+import { initializeApollo } from 'utils/apollo'
+import { RecommendedQuery } from 'graphql/queries/recommended.generated'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 const CartPage = (props: CartTemplateProps) => {
   return <Cart {...props} />
@@ -13,13 +16,22 @@ const CartPage = (props: CartTemplateProps) => {
 export default CartPage
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<RecommendedQuery>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
       cards: cardsMock,
       items: itemsMock,
       total: 'R$ 430,00',
-      recommendedGames: gamesMock,
-      recommendedHighlight: highlightMock
+      recommendedTitle: data.recommended?.section?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      )
     }
   }
 }
