55fbea1104dfb3f1992201dc06d680b4507dc41e
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-05-14 11:37:50 -0300

    feat: connecting wishlist page with api

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 
 import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
+import { RecommendedQuery } from 'graphql/queries/recommended.generated'
 
 const WishlistPage = (props: WishlistTemplateProps) => {
   return <Wishlist {...props} />
@@ -11,11 +14,20 @@ const WishlistPage = (props: WishlistTemplateProps) => {
 export default WishlistPage
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<RecommendedQuery>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
       games: gamesMock,
-      recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighlight: highlightMock
+      recommendedTitle: data.recommended?.section?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      )
     }
   }
 }
