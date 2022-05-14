1e4d2364d24ddf22e8ce6f71dbfbb2ded3e497dc
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-05-14 11:04:26 -0300

    feat: created format price utils

---
+++ src/utils/formatPrice.ts
@@ -0,0 +1,10 @@
+export default function formatPrice(price: number | bigint): string {
+  if (!price) {
+    return 'FREE'
+  }
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(price)
+}
