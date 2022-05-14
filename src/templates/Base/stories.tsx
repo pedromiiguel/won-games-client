a02f5e26c5259054a7926908c38fbe3f9687daf6
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-05-14 14:06:22 -0300

    fix: errors typescript and eslint

--- src/templates/Base/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Base from '.'
-
-export default {
-  title: 'Base',
-  component: Base
-} as Meta
-
-export const Default: Story = () => <Base />
