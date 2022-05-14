4f90e715d7194a4bd20c1af7f0fe0918c727d420
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-05-14 11:35:15 -0300

    feat: connecting home page with api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,39 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
-import bannerMock from 'components/BannerSlider/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { HomeQuery, HomeQueryVariables } from 'graphql/queries/home.generated'
+import { QUERY_HOME } from 'graphql/queries/home'
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+  const TODAY = new Date().toISOString().slice(0, 10)
+
+  const {
+    data: { banners, newGames, upcommingGames, freeGames, sections }
+  } = await apolloClient.query<HomeQuery, HomeQueryVariables>({
+    query: QUERY_HOME,
+    variables: { date: TODAY }
+  })
+
   return {
     props: {
-      banners: bannerMock,
-      newGames: gamesMock,
-      mostPopularHighlight: highlightMock,
-      mostPopularGames: gamesMock,
-      upcommingGames: gamesMock,
-      upcommingHighlight: highlightMock,
-      upcommingMoreGames: gamesMock,
-      freeGames: gamesMock,
-      freeHighlight: highlightMock
+      revalidate: 60,
+      banners: bannerMapper(banners),
+      newGamesTitle: sections?.newGames?.title,
+      newGames: gamesMapper(newGames),
+      mostPopularGamesTitle: sections?.popularGames?.title,
+      mostPopularHighlight: highlightMapper(sections?.popularGames?.highlight),
+      mostPopularGames: gamesMapper(sections?.popularGames?.games),
+      upcommingGamesTitle: sections?.upcomingGames?.title,
+      upcommingGames: gamesMapper(upcommingGames),
+      upcommingHighlight: highlightMapper(sections?.upcomingGames?.highlight),
+      freeGamesTitle: sections?.freeGames?.title,
+      freeGames: gamesMapper(freeGames),
+      freeHighlight: highlightMapper(sections?.freeGames?.highlight)
     }
   }
 }
