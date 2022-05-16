128a82dcf10e378cf9bea2f26f46fcb901d4ae05
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-05-16 11:28:59 -0300

    feat: connected with useCart hook and adding new id prop

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -1,9 +1,10 @@
-import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import { CartContextDefaultValues } from 'hooks/useCart'
+import { screen, render, fireEvent } from 'utils/test-utils'
 
 import GameItem from '.'
 
 const props = {
+  id: '1',
   img: 'https://source.unsplash.com/user/willianjusten/151x70',
   title: 'Red Dead Redemption 2',
   price: 'R$ 215,00'
@@ -11,7 +12,7 @@ const props = {
 
 describe('<GameItem />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<GameItem {...props} />)
+    render(<GameItem {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /Red Dead Redemption 2/i })
@@ -24,10 +25,26 @@ describe('<GameItem />', () => {
     expect(screen.getByText(/R\$ 215,00/i)).toBeInTheDocument()
   })
 
+  it('should render remove if them item is inside the cart and call remove', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn()
+    }
+    render(<GameItem {...props} />, { cartProviderProps })
+
+    const removeLink = screen.getByText(/remove/i)
+    expect(removeLink).toBeInTheDocument()
+
+    fireEvent.click(removeLink)
+
+    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
+  })
+
   it('should render the item with download link', () => {
     const downloadLink = 'https://www.google.com.br'
 
-    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+    render(<GameItem {...props} downloadLink={downloadLink} />)
 
     expect(
       screen.getByRole('link', { name: `Get ${props.title} here` })
@@ -42,7 +59,7 @@ describe('<GameItem />', () => {
       purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
     }
 
-    renderWithTheme(<GameItem {...props} paymentInfo={paymentInfo} />)
+    render(<GameItem {...props} paymentInfo={paymentInfo} />)
 
     expect(screen.getByRole('img', { name: paymentInfo.flag })).toHaveAttribute(
       'src',
